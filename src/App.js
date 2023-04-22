84628ceb55b6c23885078c2f9370994b52f72792
Jaceel <Jaceel@live.com.mx>
2023-04-21 20:16:24 -0700

    commit dos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 import Formulario from './componentes/Formulario/formulario';
 import Header from './componentes/Header/Header.js';
 import MiOrg from './componentes/MiOrg';
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true)
+
   return (
     <div>
       <Header />
